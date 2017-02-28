14898410ca9db966099a4a64977c99483974d33a
AlexChe96 <AlexChe96@gmail.com>
2017-02-28 12:30:53 +0200

    Transaction table create SQL command

--- src/main/resources/sql/mysql_create_tables.sql
+++ src/main/resources/sql/mysql_create_tables.sql
@@ -6,3 +6,17 @@ CREATE TABLE feedback(
   message_time TIMESTAMP NOT NULL,
   PRIMARY KEY (feedback_id)
 );
+
+CREATE TABLE transaction(
+ transaction_id  BIGINT NOT NULL,
+ login_id VARCHAR(20) NOT NULL,
+ date_time TIMESTAMP NOT NULL,
+ value DECIMAL(9,2) NOT NULL,
+ info VARCHAR(255),
+ CONSTRAINT pk_transaction_id
+ 	PRIMARY KEY (transaction_id),
+ CONSTRAINT fk_login_id
+ 	FOREIGN KEY (login_id)
+ 	REFERENCES user (login_id)
+ 	ON DELETE CASCADE
+);
