106945ca0e489b908eb0a726dd66bf00354781bf
Rymar Stanislav <rymar4ik@gmail.com>
2017-02-28 22:02:59 +0200

    Was added User and User_data query for creating table

--- src/main/resources/sql/mysql_create_tables.sql
+++ src/main/resources/sql/mysql_create_tables.sql
@@ -1,22 +1,51 @@
+CREATE TABLE user (
+  login_id        VARCHAR(20)   NOT NULL,
+  password        VARCHAR(255)  NOT NULL,
+  roles_id        INT           NOT NULL,
+  balance         DECIMAL(9, 2) NOT NULL,
+  email           VARCHAR(30)   NOT NULL,
+  last_login_date TIMESTAMP,
+  PRIMARY KEY (login_id)
+#   FOREIGN KEY (roles_id) REFERENCES roles(roles_id)
+);
 
-CREATE TABLE feedback(
-  feedback_id BIGINT NOT NULL,
-  message VARCHAR (200) NOT NULL,
-  email VARCHAR (30) NOT NULL,
-  message_time TIMESTAMP NOT NULL,
+CREATE TABLE user_data (
+  passport   VARCHAR(20) NOT NULL,
+  login_id   VARCHAR(30) NOT NULL,
+  name       VARCHAR(15) NOT NULL,
+  surname    VARCHAR(15) NOT NULL,
+  patronymic VARCHAR(15) NOT NULL,
+  gender     BOOLEAN     NOT NULL,
+  birth_day  DATETIME    NOT NULL,
+  country    VARCHAR(20),
+  city       VARCHAR(20),
+  adress     VARCHAR(40),
+  telephone  VARCHAR(25) NOT NULL,
+  PRIMARY KEY (passport),
+  FOREIGN KEY (login_id) REFERENCES user (login_id)
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE feedback (
+  feedback_id  BIGINT       NOT NULL,
+  message      VARCHAR(200) NOT NULL,
+  email        VARCHAR(30)  NOT NULL,
+  message_time TIMESTAMP    NOT NULL,
+
   PRIMARY KEY (feedback_id)
 );
 
-CREATE TABLE transaction(
- transaction_id  BIGINT NOT NULL,
- login_id VARCHAR(20) NOT NULL,
- date_time TIMESTAMP NOT NULL,
- value DECIMAL(9,2) NOT NULL,
- info VARCHAR(255),
- CONSTRAINT pk_transaction_id
- 	PRIMARY KEY (transaction_id),
- CONSTRAINT fk_login_id
- 	FOREIGN KEY (login_id)
- 	REFERENCES user (login_id)
- 	ON DELETE CASCADE
+CREATE TABLE transaction (
+  transaction_id BIGINT        NOT NULL,
+  login_id       VARCHAR(20)   NOT NULL,
+  date_time      TIMESTAMP     NOT NULL,
+  value          DECIMAL(9, 2) NOT NULL,
+  info           VARCHAR(255),
+  CONSTRAINT pk_transaction_id
+  PRIMARY KEY (transaction_id),
+  CONSTRAINT fk_login_id
+  FOREIGN KEY (login_id)
+  REFERENCES user (login_id)
+    ON DELETE CASCADE
 );
