7e53d03d8a49c04a5b1f60190107e1cd5e36c7ac
Alexander Chernoivan <AlexChe96@gmail.com>
2017-02-28 16:02:27 +0200

    Transaction create table SQL query fix

--- src/main/resources/sql/mysql_create_tables.sql
+++ src/main/resources/sql/mysql_create_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE transaction(
  transaction_id  BIGINT NOT NULL,
  login_id VARCHAR(20) NOT NULL,
  date_time TIMESTAMP NOT NULL,
- value DECIMAL(9,2) NOT NULL,
+ amount DECIMAL(9,2) NOT NULL,
  info VARCHAR(255),
  CONSTRAINT pk_transaction_id
  	PRIMARY KEY (transaction_id),
@@ -19,4 +19,5 @@ CREATE TABLE transaction(
  	FOREIGN KEY (login_id)
  	REFERENCES user (login_id)
  	ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
