18ebcd47ae9da90b8f87412dbbcdca9d62f46b4e
vova <vova@vova-CQ62-205SR-PERFECT>
2017-03-03 20:54:03 +0200

    Added insert statements for "roles" and "automat" tables

--- src/main/resources/sql/mysql_insert_data.sql
+++ src/main/resources/sql/mysql_insert_data.sql
@@ -2,3 +2,15 @@ INSERT INTO feedback(feedback_id, message, email, message_time) VALUES(1, "Hello
 INSERT INTO feedback(feedback_id, message, email, message_time) VALUES(2, "Hello everyone", vasya1488@gmail.com, '2017-01-19 03:14:07');
 INSERT INTO feedback(feedback_id, message, email, message_time) VALUES(3, "Hello everyone", vasya666@gmail.com, '2017-01-19 03:14:07');
 INSERT INTO feedback(feedback_id, message, email, message_time) VALUES(4, "Hello everyone", vasya322@gmail.com, '2017-01-19 03:14:07');
+
+INSERT INTO automat(automat_id, automat_name, description) 
+            VALUES (1, 'Slot machine','A slot machine is a casino gambling machine with three or more reels which spin when a button is pushed.');
+
+INSERT INTO roles(roles_id, role_name, description) 
+            VALUES (1, 'root', 'Administrative user, has access to everything.');
+INSERT INTO roles(roles_id, role_name, description) 
+            VALUES (2, 'moderator', 'Can edit feedbacks, comments and content.');
+INSERT INTO roles(roles_id, role_name, description) 
+            VALUES (3, 'player', 'Can sign in, operate his balance, play games, edit his own personal data, view games history and site''s content, leave comments and feedback.');
+INSERT INTO roles(roles_id, role_name, description) 
+            VALUES (4, 'guest', 'Can sign up, operate his temporary balance, view site''s content, play games and leave feedback');
