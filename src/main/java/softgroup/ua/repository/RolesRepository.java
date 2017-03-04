7cf45429aea8150fe8cd9368e59c5a969b5f5666
VovaKor <volodymyr.korobko@gmail.com>
2017-03-04 18:36:46 +0200

    Created roles repository

---
+++ src/main/java/softgroup/ua/repository/RolesRepository.java
@@ -0,0 +1,15 @@
+package softgroup.ua.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import softgroup.ua.jpa.RolesEntity;
+
+import java.util.List;
+
+/**
+ * Created by Вова on 04.03.2017.
+ */
+public interface RolesRepository extends CrudRepository<RolesEntity, Long> {
+    public List <RolesEntity> findByRoleName (String roleName);
+    public List <RolesEntity> findByRolesId (int rolesId);
+    public List <RolesEntity> findAll();
+}
