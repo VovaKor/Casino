33ab062765d3994daac60a234b6cbb0ca55a10f7
Anjey-13 <Anjey-13@mail.ru>
2017-03-10 15:05:39 +0200

    Fixed AutomatRepository. Now extends from JpaRepository

--- Casino/src/main/java/softgroup/ua/repository/AutomatRepository.java
+++ Casino/src/main/java/softgroup/ua/repository/AutomatRepository.java
@@ -4,11 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import softgroup.ua.jpa.AutomatEntity;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by Вова on 04.03.2017.
  */
-public interface AutomatRepository extends CrudRepository <AutomatEntity, Long> {
+public interface AutomatRepository extends JpaRepository <AutomatEntity, Long> {
     public List <AutomatEntity> findByAutomatId (int automatId);
     public List <AutomatEntity> findByAutomatName (String automatName);
     public List <AutomatEntity> findAll();
