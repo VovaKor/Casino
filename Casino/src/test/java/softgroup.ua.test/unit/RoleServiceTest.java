16636ef6b6db15e67301bb51c09bbdb0e96776d8
VovaKor <volodymyr.korobko@gmail.com>
2017-03-12 23:13:59 +0200

    Added tests for CRUD

--- Casino/src/test/java/softgroup.ua.test/unit/RoleServiceTest.java
+++ Casino/src/test/java/softgroup.ua.test/unit/RoleServiceTest.java
@@ -3,10 +3,12 @@ package softgroup.ua.test.unit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import softgroup.ua.jpa.RoleEntity;
 import softgroup.ua.service.RoleService;
 
 /**
@@ -15,14 +17,51 @@ import softgroup.ua.service.RoleService;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RoleServiceTest {
+
+
     @Autowired
     RoleService roleService;
+    private RoleEntity roleEntity;
+    private Integer roleId;
     @Before
     public void setUp() throws Exception {
+        roleId = 100;
+        roleEntity = new RoleEntity(roleId,"test","Test role");
     }
 
     @After
     public void tearDown() throws Exception {
+    }
+    @Test
+    public void getRoleById() throws Exception {
+        RoleEntity testRoleEntity = roleService.getRoleById(3);
+        assertNotNull("Role not found",testRoleEntity);
+    }
+
+    @Test
+    public void addAndDeleteRole() throws Exception {
+        roleService.addRole(roleEntity);
+        RoleEntity testRoleEntity = roleService.getRoleById(roleId);
+        assertNotNull("Test role not found.",testRoleEntity);
+        roleService.deleteRole(roleId);
+        testRoleEntity = roleService.getRoleById(roleId);
+        assertNull("Test role not deleted.",testRoleEntity);
+    }
+
+    @Test
+    public void updateAndDeleteRole() throws Exception {
+        roleService.addRole(roleEntity);
+        RoleEntity testRoleEntity = roleService.getRoleById(roleId);
+        assertNotNull("Test role not found.",testRoleEntity);
+        testRoleEntity.setRoleName("updatedTestName");
+        testRoleEntity.setDescription("New test description");
+        roleService.updateRole(testRoleEntity);
+        testRoleEntity = roleService.getRoleById(roleId);
+        assertEquals("Role name not updated","updatedTestName",testRoleEntity.getRoleName());
+        assertEquals("Role description not updated","New test description",testRoleEntity.getDescription());
+        roleService.deleteRole(roleId);
+        testRoleEntity = roleService.getRoleById(roleId);
+        assertNull("Test role not deleted.",testRoleEntity);
     }
 
     @Test
