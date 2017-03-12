2ef9975ada05f51571cb83cc6e9f7dd0822c833c
VovaKor <volodymyr.korobko@gmail.com>
2017-03-12 15:17:43 +0200

    Commented unnecessary method findAll, switched to JpaRepository

--- Casino/src/main/java/softgroup/ua/repository/RolesRepository.java
+++ Casino/src/main/java/softgroup/ua/repository/RolesRepository.java
@@ -1,5 +1,6 @@
 package softgroup.ua.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import softgroup.ua.jpa.RolesEntity;
 
@@ -8,8 +9,8 @@ import java.util.List;
 /**
  * Created by Вова on 04.03.2017.
  */
-public interface RolesRepository extends CrudRepository<RolesEntity, Long> {
+public interface RolesRepository extends JpaRepository<RolesEntity, Long> {
     public List <RolesEntity> findByRoleName (String roleName);
     public List <RolesEntity> findByRolesId (int rolesId);
-    public List <RolesEntity> findAll();
+    //public List <RolesEntity> findAll();
 }
