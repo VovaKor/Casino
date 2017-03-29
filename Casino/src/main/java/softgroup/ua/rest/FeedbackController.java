2dcf5b7053b5e61eb94df2d02480c35a5ea1742e
panteliienko.danylo <panteliienko.danylo@gmail.com>
2017-03-29 20:54:56 +0300

    refactor Feedback Mapper and add FeedbackControlelr

---
+++ Casino/src/main/java/softgroup/ua/rest/FeedbackController.java
@@ -0,0 +1,74 @@
+package softgroup.ua.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import softgroup.ua.api.AddFeedbackRequest;
+import softgroup.ua.api.FeedbackListReply;
+import softgroup.ua.jpa.Feedback;
+import softgroup.ua.service.FeedbackMapper;
+import softgroup.ua.service.FeedbackService;
+
+
+@RestController
+public class FeedbackController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FeedbackController.class);
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @Autowired
+    private FeedbackMapper feedbackMapper;
+
+    @RequestMapping(path = "/feedback/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public FeedbackListReply getAllFeedbacks() {
+        FeedbackListReply reply = new FeedbackListReply();
+        for (Feedback f : feedbackService.getAllFeedback()) {
+            reply.feedbacks.add(feedbackMapper.fromInternal(f));
+        }
+        return reply;
+    }
+
+    @RequestMapping(path = "/feedback/add",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public FeedbackListReply addFeedback(@RequestBody AddFeedbackRequest addFeedbackRequest) {
+        FeedbackListReply reply = new FeedbackListReply();
+        try {
+            Feedback feedback = feedbackService.addFeedback(feedbackMapper.toInternal(addFeedbackRequest.feedback));
+            reply.feedbacks.add(feedbackMapper.fromInternal(feedback));
+        } catch (Exception e) {
+            reply.retcode = -1;
+            reply.error_message = e.getMessage();
+            logger.error("Error adding feedback. Exception: " + e.getMessage(), e);
+        }
+        return reply;
+    }
+
+    @RequestMapping(path = "/feedback/byId/{feedbackId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public FeedbackListReply getFeedbackById(@PathVariable Long feedbackId) {
+        FeedbackListReply reply = new FeedbackListReply();
+        reply.feedbacks.add(feedbackMapper.fromInternal(feedbackService.findFeedbackById(feedbackId)));
+        return reply;
+    }
+
+    @RequestMapping(path = "/feedback/delete/{feedbackId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public FeedbackListReply deleteFeedbackById(@PathVariable Long feedbackId) {
+        FeedbackListReply reply = new FeedbackListReply();
+        try {
+            feedbackService.deleteFeedback(feedbackId);
+        } catch (Exception e) {
+            reply.retcode = -1;
+            reply.error_message = e.getMessage();
+            logger.error("Error delete feedback. Exception: " + e.getMessage(), e);
+        }
+        return reply;
+
+
+    }
+
+}
--- Casino/src/main/java/softgroup/ua/service/FeedbackMapper.java
+++ Casino/src/main/java/softgroup/ua/service/FeedbackMapper.java
@@ -2,10 +2,11 @@ package softgroup.ua.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-//import softgroup.ua.api.FeedbackAPI;
 import softgroup.ua.api.FeedbackAPI;
 import softgroup.ua.jpa.Feedback;
 import softgroup.ua.repository.FeedbackRepository;
+
+//import softgroup.ua.api.FeedbackAPI;
 
 @Component
 public class FeedbackMapper {
@@ -13,9 +14,9 @@ public class FeedbackMapper {
     @Autowired
     private FeedbackRepository feedbackRepository;
 
-    public FeedbackAPI fromInternal(Feedback feedback){
+    public FeedbackAPI fromInternal(Feedback feedback) {
         FeedbackAPI feedbackAPI = null;
-        if(feedback != null){
+        if (feedback != null) {
             feedbackAPI = new FeedbackAPI();
             feedbackAPI.feedbackId = feedback.getFeedbackId().toString();
             feedbackAPI.message = feedback.getMessage();
@@ -25,12 +26,12 @@ public class FeedbackMapper {
         return feedbackAPI;
     }
 
-    public Feedback toInternal(FeedbackAPI feedbackAPI){
+    public Feedback toInternal(FeedbackAPI feedbackAPI) {
         Feedback feedback = null;
-        if(feedbackAPI.feedbackId != null){
+        if (feedbackAPI.feedbackId != null) {
             feedback = feedbackRepository.findOne(Long.parseLong(feedbackAPI.feedbackId));
         }
-        if(feedback == null){
+        if (feedback == null) {
             feedback = new softgroup.ua.jpa.Feedback();
         }
         feedback.setFeedbackId(Long.parseLong(feedbackAPI.feedbackId));
