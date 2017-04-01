945634326fb0a6c7a9f3a6832f2539e3d5e3eb4e
VovaKor <volodymyr.korobko@gmail.com>
2017-04-02 00:48:05 +0300

    Added custom game exception

---
+++ Casino/src/main/java/softgroup/ua/service/exception/GameException.java
@@ -0,0 +1,10 @@
+package softgroup.ua.service.exception;
+
+/**
+ * Created by Вова on 01.04.2017.
+ */
+public class GameException extends Exception {
+    public GameException(String s) {
+        super(s);
+    }
+}
